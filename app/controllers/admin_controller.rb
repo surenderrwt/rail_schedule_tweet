6846206a1b9d25e75ffd5d5e7e2bef1f5ef72385
surender rawat <rawat.surender87@gmail.com>
2020-02-05 03:33:05 +0530

    fix indent

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,26 +1,26 @@
 class AdminController < ApplicationController
 	before_action :set_user, except:[:users]
 	before_action :authenticate_user!
-  
-  def users
-  	@users = User.where(["role_id = ?", 1])
-  end
-
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to action: "users"
-  	end
+	
+	def users
+		@users = User.where(["role_id = ?", 1])
 	end
 
-  def set_user
-    @user=   User.find(params[:id])
-  end
+	def edit
+	end
 
-  def user_params
-    params.require(:user).permit(:email, :activate)
-  end
+	def update
+		if @user.update(user_params)
+			redirect_to action: "users"
+		end
+	end
+
+	def set_user
+		@user=   User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:email, :activate)
+	end
 
 end
