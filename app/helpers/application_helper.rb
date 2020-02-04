4c193fde9e10a6f8c19644853517dccf4ddba137
surender rawat <rawat.surender87@gmail.com>
2020-02-05 02:45:21 +0530

    Add helper functions for links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def twitter_authenticate_status
+		if current_user 
+		    if current_user.access_token.nil?
+		       '<h3> Disconnected</h3>'.html_safe
+		    else
+		        '<h3> Connected </h3>'.html_safe
+		    end
+	    end 
+	end
+
+	def link_to_twitter_authenticate
+		if current_user
+			if current_user.access_token.nil?
+				link_to "Login with twitter", "oauth/request"
+			end
+		end
+	end
 end
